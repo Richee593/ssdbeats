43789e191385e8725bc88bd1034da7ddc8a4b9d7
SSD Beats <ssdbeats@local.dev>
2026-04-19 12:29:21 +0200

    add sitemap

--- app.js
+++ app.js
@@ -146,11 +146,16 @@ app.use((req, res, next) => {
   next();
 });
 
-//sitemap
+
+
 app.get("/sitemap.xml", (req, res) => {
   res.sendFile(__dirname + "/sitemap.xml");
 });
 
+
+app.get("/robots.txt", (req, res) => {
+  res.sendFile(__dirname + "/robots.txt");
+});
 
 
 /* =========================
